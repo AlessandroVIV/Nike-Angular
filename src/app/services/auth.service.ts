e0c9c19092f83e3db54368a10aa5ab47e2cc013d
AlessandroVIV <alex01.vivaldi@gmail.com>
2025-03-13 15:18:29 +0100

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,76 +10,68 @@ export class AuthService {
 
   private readonly TOKEN_KEY = 'auth_token';
   
-  private readonly USERS_URL = 'http://localhost:3000/utenti'; 
+  private readonly USERS_URL = 'http://localhost:8080';
 
   constructor(private router: Router, private http: HttpClient) {}
 
-  register(NomeUtente: string, password: string): void{
-   
-    this.http.get<any[]>(this.USERS_URL).subscribe((users) => {
+  register(NomeUtente: string, password: string): void {
 
-      console.log('Utenti letti dal server:', users); 
+    const newUser = { username: NomeUtente, password };
   
-      const userExists = users.some((user) => user.nome === NomeUtente);
-  
-      if(userExists) 
-      {
-        console.log('Questo utente esiste già!');
-      } 
-      else 
-      {
-        const newUser = {nome: NomeUtente, password};
+    this.http.post(`${this.USERS_URL}/register`, newUser, { responseType: 'text' }).subscribe({
 
-        this.http.post(this.USERS_URL, newUser).subscribe(() => {
+      next: (response) => {
+        console.log('Risposta del server:', response);
+        this.router.navigate(['/login']);
+      },
 
-          console.log('Utente aggiunto:', newUser); 
-          this.router.navigate(['/login']); 
-          
-        });
-
+      error: (err) => {
+        console.error('Errore nella registrazione:', err);
+        alert('Errore nella registrazione!');
       }
 
     });
-
+    
   }
   
-  login(NomeUtente: string, password: string): Promise<boolean>{
+  login(NomeUtente: string, password: string): Promise<boolean> {
 
     return new Promise((resolve, reject) => {
 
-      this.http.get<any[]>(this.USERS_URL).subscribe((users) => {
-
-        console.log('Utenti presenti:', users);
+      const loginUser = { username: NomeUtente, password };
   
-        const user = users.find(
-          (user) => user.nome === NomeUtente && user.password === password
-        );
-  
-        if(user) 
-        {
-          const token = btoa(`${NomeUtente}:${password}`);
-          localStorage.setItem(this.TOKEN_KEY, token);
-          this.router.navigate(['/area-riservata']);
-          resolve(true); 
-        }
-        else 
-        {
-          resolve(false); 
+      this.http.post<{ secretKey: string }>(`${this.USERS_URL}/login`, loginUser).subscribe({
+
+        next: (response) => {
+
+          if(response.secretKey) 
+          {
+            localStorage.setItem(this.TOKEN_KEY, response.secretKey);
+            this.router.navigate(['/area-riservata']);
+            resolve(true);
+          } 
+          else 
+          {
+            resolve(false);
+          }
+        },
+
+        error: (err) => {
+          console.error('Errore nel login:', err);
+          reject(false);
+
         }
 
-      }, (error) => {
-        console.error('Errore nella richiesta:', error);
-        reject(false); 
       });
 
     });
     
   }
   
-  isAuthenticated(): boolean{
-    return !!localStorage.getItem(this.TOKEN_KEY); 
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem(this.TOKEN_KEY);
   }
-
+  
   logout(): void{
     localStorage.removeItem(this.TOKEN_KEY); 
     this.router.navigate(['/']); 
